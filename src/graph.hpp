61dca805da9e578753270ddaecaef3b9f5284d40
diegotaveras <diegoat2@illinois.edu>
2022-11-10 00:01:13 -0600

    added possible graph list implementation

--- src/graph.hpp
+++ src/graph.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <list>
 
 class Graph {
 public:
@@ -14,5 +15,36 @@ private:
     std::map<std::string, bool>& GetAdjacencyMap(const std::string& source);
     std::map<std::string, std::map<std::string, bool>> matrix;
     
+};
+
+
+
+// list representation in case it's needed.
+class GraphLists {                                          
+    public:
+        GraphLists(const std::string& file);
+
+        struct Vertex {
+            Vertex(std::string d, std::list<Vertex*> a) : data(d), adjacent(a) {}
+
+            std::string data;
+            std::list<Vertex*> adjacent;
+            
+        };
+
+        struct Edge {
+            Edge(Vertex* s, Vertex* e) : start(s), end(e) {}
+            Edge(Vertex* s, Vertex* e, int w) : start(s), end(e), weight(w) {}
+
+            int weight = 0;
+            Vertex* start = NULL;
+            Vertex* end = NULL;
+        };
+
+
+    private: 
+        std::vector<Vertex*> vertices;
+        std::list<Edge> edges;
+
 };
 
