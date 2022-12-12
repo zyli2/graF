25459b39d26e4358606857d0baa88a6984ee63f9
zyli2 <zyli2@illinois.edu>
2022-12-11 22:51:30 -0600

    updated readme and main.cpp

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # graF
+
+# Github Organization
+
+# graF
+Stores the graph drawing image produced by using the drawing functions
+The graph drawing output is named output.png here
+
+# data
+Location of all the data used for producing a graph
+
+# entry
+main.cpp is here
+
+# src
+Location of all of our written code
+
+# final-deliverables
+Location of our written report, presentation powerpoint, presentation link
+
+# tests
+Location of our test cases and where we store the images produced by our test cases
+
+# Running Instructions
+
+Makefile
+
+Make sure you're in the graF directory first and have your docker opened
+
+1) mkdir build
+
+2) cd build
+
+3) cmake ..
+
+Running the code
+
+If you want to run our program with your dataset, make sure your dataset is in a csv format and it is similar to our data_subreddit_title.csv file which is in our data directory. The first value should be the source vertex and the second value should be the target vertex (the destination your source vertex reaches).
+
+Make sure you're in the build directory and follow the steps below
+
+1) make
+
+2) ./main
+
+3) follow the rest of the steps in your terminal. Our program will guide you
+
+Test cases
+
+Make sure you're in the build directory and follow the steps below
+
+1) make test
+
+2) ./test
+
+BFS Test - looks at the accuracy for producing the shortest path for two vertices by evaluating the actual path and path length
+
+Betweenness Centrality test - looks at whether or not our function returns the expected betweenness centrality we calculated by hand
+
+Degree Centrality test - looks at whether or not our function returns the expected betweenness centrality we calculated by hand
+
+Graph drawing test - idk
+
+Data parsing test - checks if the correct amount of vertices exist and if there are a correct amount of
+edges and correct edges
--- entry/main.cpp
+++ entry/main.cpp
@@ -7,6 +7,99 @@
 
 using namespace cs225;
 int main() {
+
+  char decision;
+
+  std::cout << "Do you want to see how our program on our reddit dataset or yours? Enter o for ours or y for yours" << std::endl;
+  std::cout << "Enter o or y:";
+
+  std::cin >> decision;
+
+  if (decision == "y") {
+
+    std::string data = "";
+
+    std::cout << "Enter your dataset's name: ";
+
+    getline(cin, data);
+
+    std::string dest = "../data/";
+
+    dest += data;
+
+    Graph graph(dest);
+
+    char graf;
+    std::cout << "Do you want to produce a graph, find the vertex with the highest betweenness centrality or find the shortest path between two vertices?" << std::endl;
+    std::cout << "Enter g for graph, b for betweenness centrality or p for shortest path";
+    std::cin >> graf;
+
+    if (graf == 'g') {
+      std::cout << "producing image..." << std::endl;
+      // enter the graph drawing code
+
+      std::cout << "image complete! it is in the graF directory and it's named output.png" << std::endl;
+    } else if (graf == 'p') {
+      std::string vertex1;
+      std::string vertex2;
+
+      std::cout << "Enter vertex 1";
+
+      getline(cin, vertex1);
+
+      std::cout << "Enter vertex 2";
+
+      getline(cin, vertex2);
+
+      std::cout << "if the output is blank, that means one of the vertices does not exist in your dataset" << std::endl;
+
+      std::cout << "shortest path is: "<< graph.BFS(vertex1, vertex2) << std::endl;
+    } else {
+      // enter betweenness centrality code
+    }
+
+    std::cout << "The program has finished running. To run the program again, enter ./main in the terminal" << std::endl;
+    std::cout << "thank you!" << std::endl;
+
+  } else if (decision == 'o') {
+
+    Graph graph("../data_source_title_unrepeated.csv");
+
+    char graf
+    std::cout << "Do you want to produce a graph, find the vertex with the highest betweenness centrality or find the shortest path between two vertices?" << std::endl;
+    std::cout << "Enter g for graph, b for betweenness centrality or p for shortest path";
+    std::cin << graf
+
+    if (graf == 'g') {
+      std::cout << "producing image..." << std::endl;
+      // enter the graph drawing code
+
+      std::cout << "image complete! it is in the graF directory and it's named output.png" << std::endl;
+    } else if (graf = 'p'){
+      std::string vertex1;
+      std::string vertex2;
+
+      std::cout << "Enter vertex 1";
+
+      getline(cin, vertex1);
+
+      std::cout << "Enter vertex 2";
+
+      getline(cin, vertex2);
+
+      std::cout << "if the output is blank, that means one of the vertices does not exist in your dataset" << std::endl;
+
+      std::cout << "shortest path is: " << graph.BFS(vertex1, vertex2) << std::endl;
+    } else {
+      // enter betweenness centrality code
+    }
+
+    std::cout << "The program has finished running. To run the program again, enter ./main in the terminal" << std::endl;
+    std::cout << "thank you!" << std::endl;
+
+  } else {
+    std::cout << "Invalid input. Please run the program again by entering in the terminal ./main" << std::endl;
+  }
   
 
   Graph graph("../data_source_title_unrepeated.csv");
